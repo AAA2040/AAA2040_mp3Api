af0763a7cf7451862aae772cbb2989e199bd6313
seulgiLee113 <cktncth55@gmail.com>
2025-07-17 14:44:43 +0900

    gpu 사용 코드로 수정

--- mp3Separate.py
+++ mp3Separate.py
@@ -2,13 +2,18 @@ import os
 import demucs.separate
 import shlex
 
-def mp3_separate(uriId):
+def mp3_separate(uriId, device='cuda'):
 
     ffmpeg_dir = r'ffmpeg-master-latest-win64-gpl-shared\bin'
     os.environ['PATH'] = ffmpeg_dir + os.pathsep + os.environ.get('PATH', '')
 
     if os.path.exists(f'separated/htdemucs/{uriId}/no_vocals.mp3'):
-        print('already separated')
         return
 
-    demucs.separate.main(shlex.split(f'--mp3 --two-stems vocals "mp3Raw/{uriId}.mp3"'))
+    try:
+        demucs.separate.main(shlex.split(f'--mp3 --two-stems vocals --device {device} "mp3Raw/{uriId}.mp3"'))
+    except Exception:
+        if device == 'cuda':
+            demucs.separate.main(shlex.split(f'--mp3 --two-stems vocals --device cpu "mp3Raw/{uriId}.mp3"'))
+        else:
+            pass
